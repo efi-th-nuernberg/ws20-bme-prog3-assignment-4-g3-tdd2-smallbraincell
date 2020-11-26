e80e173d8e5f466e964a8c2ad09f103ef3a1f8ce
SelinSchmitt <73957323+SelinSchmitt@users.noreply.github.com>
2020-11-26 19:12:57 +0100

    Final Commit WOhoo

--- TriangleChecker.java
+++ TriangleChecker.java
@@ -48,19 +48,19 @@ public class TriangleChecker {
 
   // Analyse der Dreiecksart
   public static TriangleType checkTriangle(float a, float b, float c) {
+      if (((a > 0) && (b > 0) && (c > 0)) && ((a + b >= c) && (b + c >= a ) && (a + c >= b))) {        
+
       if ((a > 0) && (b > 0) && (c > 0)) {        
+
         if (a == b && b == c) { 
           return TriangleType.EQUILATERAL; 
-
         } else if ( a == b || a == c || b == c) { 
           return TriangleType.ISOSCELES; 
+        } else {
+        return TriangleType.NORMAL; 
         }
-
-      return TriangleType.NORMAL; 
-      
       } else {
          return TriangleType.NONE; 
       }
-
   }
 }
